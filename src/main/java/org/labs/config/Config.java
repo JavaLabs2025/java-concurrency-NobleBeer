c3c37bff6e4ebf00d0e03d01011b3a381eed9a6e
Благородная Людмила <momonga.nim@gmail.com>
2025-09-20 16:17:45 +0300

    LAB-1 Добавление logback.xml, актуализация конфигурационных параметров

--- src/main/java/org/labs/config/Config.java
+++ src/main/java/org/labs/config/Config.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 public class Config {
     public static int DEVELOPER_COUNT = 7;
     public static int MAX_WAIT_MS = 1;
-    public static int DINNER_DURATION_IN_MS = 1000;
-    public static int DISH_COUNT = 1000;
+    public static int DINNER_DURATION_IN_MS = 10;
+    public static int DISH_COUNT = 1000000;
     public static int WAITER_COUNT = 2;
 }
